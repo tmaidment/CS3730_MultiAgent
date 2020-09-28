406d08bbde4ca946eea7167a687b24f5d40593fc
Anthony Sicilia <anthonysicilia@Anthonys-MBP-2.lan>
2020-09-28 16:59:56 -0400

    Adds spatial features

--- src/datasets/dialog.py
+++ src/datasets/dialog.py
@@ -3,6 +3,13 @@ import torch
 import json
 import os
 
+def unit_normalize(x, max_, min_):
+    """
+    Returns x normalized to the range -1,1
+    """
+    x_zero_one = (x - min_) / (max_ - min_)
+    return 2 * x_zero_one - 1
+
 class Dialog(torch.utils.data.Dataset):
 
     def __init__(self, image_loc, json_loc):
@@ -14,13 +21,28 @@ class Dialog(torch.utils.data.Dataset):
         return len(self.json_objs)
     
     def __getitem__(self, i):
-        obj = self.json_objs[i]
-        img_path = os.path.join(self.image_loc, obj['image']['file_name'])
+        x = self.json_objs[i]
+        img_path = os.path.join(self.image_loc, x['image']['file_name'])
         img = Image.open(img_path)
-        obj['image']['raw'] = img
+        x['image']['raw'] = img
 
         # TODO:
-        # normalize object bboxs to [0-1]?
         # encode questions and answers as integers?
-
-        return obj
+        
+        for i in range(len(x['objects'])):
+            # [x,y,width,height]
+            # x, y: the upper-left coordinates of the bounding box
+            [x_tl, y_tl, w, h] = x['objects']['bbox']
+            xmin = unit_normalize(x_tl, w, 0)
+            xmax = unit_normalize(x_tl + w, w, 0)
+            xcenter = unit_normalize(x_tl + w // 2, w, 0)
+            ymin = unit_normalize(y_tl, h, 0)
+            ymax = unit_normalize(y_tl + h, h, 0)
+            ycenter = unit_normalize(y_tl + h // 2, h, 0)
+            wbox = unit_normalize(w, w, 0)
+            hbox = unit_normalize(h, h, 0)
+            # spatial features used by https://arxiv.org/pdf/1703.05423.pdf
+            x['objects']['spatial'] = [xmin, ymin, xmax, 
+                ymax, xcenter, ycenter, wbox, hbox]
+                
+        return x
